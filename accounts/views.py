c80fefe3bb80187896f6f7e40dbaab76c6ad28a0
Noorul Ahsan <noorulahsanpp@gmail.com>
2020-03-30 20:01:57 +0530

    signup and signin done! logout aint working. so commented its code in base page

--- accounts/views.py
+++ accounts/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib import auth
 
+
 # Create your views here.
 
 def signup(request):
     if request.method == 'POST':
-        #User has info and wants an account
+        # User has info and wants an account
         if request.POST['password'] == request.POST['cpassword']:
             try:
                 user = User.objects.get(username=request.POST['username'])
@@ -17,10 +18,24 @@ def signup(request):
                 return redirect('home')
         else:
             return render(request, 'accounts/signup.html', {'error': 'Password does not match'})
-    else :
-        #User wants to entet info
+    else:
+        # User wants to enter info
         return render(request, 'accounts/signup.html')
 
 
 def login(request):
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+        if user is not None:
+            auth.login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'accounts/login.html', {'error': 'username or password incorrect'})
+    else:
+        return render(request, 'accounts/login.html')
+
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
--- product_hunt/settings.py
+++ product_hunt/settings.py
@@ -130,3 +130,6 @@ STATICFILES_DIRS = [
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
+
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
